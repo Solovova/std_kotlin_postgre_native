bd06570b6b9059d0ee9debf7be675db27566d0d1
Solovova <vsolo0@gmail.com>
2021-01-30 00:30:34 +0200

    0.01 Test connect

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,19 +20,11 @@ repositories {
 }
 
 dependencies {
-    // Align versions of all Kotlin components
+    implementation("org.postgresql:postgresql:42.2.18")
     implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
-
-    // Use the Kotlin JDK 8 standard library.
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-
-    // This dependency is used by the application.
     implementation("com.google.guava:guava:29.0-jre")
-
-    // Use the Kotlin test library.
     testImplementation("org.jetbrains.kotlin:kotlin-test")
-
-    // Use the Kotlin JUnit integration.
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
 }
 
---
+++ app/src/main/kotlin/std_kotlin_postgre_native/db/PostGreConnection.kt
@@ -0,0 +1,44 @@
+package std_kotlin_postgre_native.db
+
+import java.sql.DriverManager
+import java.sql.Connection
+import java.sql.SQLException
+
+class PostGreConnection {
+    companion object {
+        private const val DB_URL: String = "jdbc:postgresql://127.0.0.1:5432/solotest"
+        private const val USER: String = "postgres"
+        private const val PASS: String = "vbwqu1pa"
+
+        fun testConnection() {
+            println("Testing connection to PostgreSQL JDBC")
+
+            try {
+                Class.forName("org.postgresql.Driver");
+            } catch (e: ClassNotFoundException) {
+                println("PostgreSQL JDBC Driver is not found. Include it in your library path ")
+                e.printStackTrace();
+                return;
+            }
+
+            println("PostgreSQL JDBC Driver successfully connected");
+            val connection: Connection?
+
+            try {
+                connection = DriverManager
+                    .getConnection(DB_URL, USER, PASS);
+
+            } catch (e: SQLException) {
+                println("Connection Failed");
+                e.printStackTrace();
+                return;
+            }
+
+            if (connection != null) {
+                println("You successfully connected to database now");
+            } else {
+                println("Failed to make connection to database");
+            }
+        }
+    }
+}
